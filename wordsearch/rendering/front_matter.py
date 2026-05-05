624cf12279753b74a80bade31ae0293ed3f4cefc
Nachobt98 <118307668+Nachobt98@users.noreply.github.com>
2026-05-06 00:51:11 +0200

    Import front matter background from rendering package

--- wordsearch/rendering/front_matter.py
+++ wordsearch/rendering/front_matter.py
@@ -21,7 +21,7 @@ from wordsearch.constants_and_layout import (
     wordlist_font_size as WORDLIST_FONT_SIZE,
     title_font_size as TITLE_FONT_SIZE,
 )
-from wordsearch.image_rendering import BACKGROUND_PATH
+from wordsearch.rendering.backgrounds import BACKGROUND_PATH
 from wordsearch.rendering.common import (
     draw_centered_text,
     load_font,
