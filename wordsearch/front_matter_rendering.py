1da248eebcb9553d5212258dff091f84564653b0
Nachobt98 <118307668+Nachobt98@users.noreply.github.com>
2026-05-06 01:10:49 +0200

    Remove legacy front matter rendering wrapper

--- wordsearch/front_matter_rendering.py
+++
@@ -1,9 +0,0 @@
-"""Backward-compatible wrapper for front matter rendering."""
-
-from wordsearch.rendering.front_matter import (
-    render_instructions_page,
-    render_table_of_contents,
-)
-
-
-__all__ = ["render_instructions_page", "render_table_of_contents"]
